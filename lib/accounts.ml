6dba6cf5db8116db734bb0c3bd36738b7feda0ac
Hrishabh Ayush <ha385@cornell.edu>
2024-10-29 13:19:15 -0400

    Implemented task.ml

--- lib/accounts.ml
+++ lib/accounts.ml
@@ -1,4 +1,23 @@
-module Account = struct end
+module Account = struct
+  type role =
+    | Doctor
+    | Pharmacist
+    | Patient
+
+  type t = {
+    role : role;
+    username : string;
+    password : string;
+    task_list : int list;
+  }
+
+  let role_display = function
+    | Doctor -> "Doctor"
+    | Pharmacist -> "Pharmacist"
+    | Patient -> "Patient"
+  
+  
+end
 
 module Pharmacist = struct
   include Account
