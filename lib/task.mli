eac0f228ddd4e211b66c6cc87d4ae8ea4ef16ec4
Hrishabh Ayush <ha385@cornell.edu>
2024-12-11 22:55:38 -0500

    test_display_tasks_from_ids and test_string_to_task_ids added to the testing file for task.ml

--- lib/task.mli
+++ lib/task.mli
@@ -1,6 +1,15 @@
 type t
+type voteTally
 
 val load_tasks_from_csv : string -> t list
 val display_tasks_from_ids : Csv.t -> int list -> string
 val display_tasks_without_votes : Csv.t -> int list -> string
 val string_to_task_ids : string -> int list
+val task_id : t -> int
+val diagnosis : t -> string
+val prescription : t -> string
+val vote_count : t -> voteTally
+val yes_votes : voteTally -> int
+val yes_voters : voteTally -> string list
+val no_votes : voteTally -> int
+val no_voters : voteTally -> string list
